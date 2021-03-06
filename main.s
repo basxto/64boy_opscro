c9161c34a7995c8e8b128966dca0947b761a99bb
Sebastian Riedel <git@basxto.de>
2021-03-06 05:00:06 +0100

    Fix alternator

--- main.s
+++ main.s
@@ -3,7 +3,6 @@ INCLUDE "hardware.inc/hardware.inc"
 SECTION "Entry Point", ROM0[$38]
     ; we assume $FF at $100
 init:
-    di
     ; must be >=144
     ; should be 0xX1 for nicer numbers later
     ld b, $91
@@ -13,12 +12,17 @@ init:
     ; returns with A=0
     ; set palette index #0
     ldh [rBCPS], a
+    ld h, b
     ; we are at the end of 64B
     jr main
 
 
-SECTION "MAIN", ROM0[$14]
+SECTION "MAIN", ROM0[$13]
 main:
+    ; a: $00
+    ; b: $90
+    ; c: $91
+    ; registers are at ^^^
     ; write first byte of first color
     ; of first cgb palette
     ; palette is assumed to be set all white
@@ -29,7 +33,7 @@ main:
     ldh [rLCDC], a
     
     ; destination hl=0x9000
-    ld h, b
+    ;ld h, b ; loaded at entry point
     ld l, a
     ; source de=0x0000
     ld d, a
@@ -41,8 +45,8 @@ main:
     ld h, $98 
     ;ld a, 12
     ; b is set to 0
-    ;rst $0 ; call alternator
-    ;rst $0 ; call alternator
+    rst $0 ; call alternator
+    rst $0 ; call alternator
     ; enable stat
     ld a, LCDCF_ON | LCDCF_BGON
     ldh [rLCDC], a
@@ -65,11 +69,12 @@ main:
     jr .infloop
 SECTION "WAITLY", ROM0[$30]
     ; wait for rLY == B
-    ; returns with A = 0
+    ; returns with A = C = 0
 waitly:
     ldh a, [rLY]
     sub b
     jr NZ, waitly
+    ld c, b
     dec b
     ret
 
@@ -78,10 +83,10 @@ SECTION "ALT", ROM0[$0]
 ; de: source
 ; b: amount (0 is 256 times)
 alternator:
+    xor a
     ld [hl+], a ; 1
     cpl
     ld [hl+], a ; 1
-    cpl
     dec b ; 1
     jr NZ, alternator ; 2
     ret ; 1
@@ -93,9 +98,6 @@ vmemcpy:
     ; read
     ld a, [de] ; 1
     inc de ; 1
-    ; model output a bit
-    ; since we use machine code for images
-    and %11111100 ; 2
     ; write as 0.5BPP
     ld [hl+], a ; 1
     ld [hl+], a ; 1
@@ -104,6 +106,8 @@ vmemcpy:
     dec b ; 1
     ; loop while b != 0
     jr NZ, vmemcpy ; 2
+    ; clean up l
+    ld  l, b
     ret ; 1
 
 SECTION "limit64B", ROM0[$40]
